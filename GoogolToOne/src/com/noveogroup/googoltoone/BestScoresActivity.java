09eb24a5af596332fc33d251a4593e6ab6d843bf
Anatoly Svichkarev <tolik0393@mail.ru>
2014-04-18 14:34:00 +0700

    add layout for bestscores

---
+++ GoogolToOne/src/com/noveogroup/googoltoone/BestScoresActivity.java
@@ -0,0 +1,14 @@
+package com.noveogroup.googoltoone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by anatoly on 20.04.14.
+ */
+public class BestScoresActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.best_scores);
+    }
+}
