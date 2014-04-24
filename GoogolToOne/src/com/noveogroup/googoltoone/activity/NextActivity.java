0b6f0442e674292fdb6f37e9f5276742d1fb2b3f
Anatoly Svichkarev <tolik0393@mail.ru>
2014-04-24 16:02:39 +0700

    implement first fragment with Score for enter_query

--- GoogolToOne/src/com/noveogroup/googoltoone/activity/NextActivity.java
+++ GoogolToOne/src/com/noveogroup/googoltoone/activity/NextActivity.java
@@ -1,12 +1,24 @@
 package com.noveogroup.googoltoone.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.TextView;
 import com.noveogroup.googoltoone.R;
+import com.noveogroup.googoltoone.ScoreUpdater;
 import com.noveogroup.googoltoone.fragment.QueryFragment;
 
-public class NextActivity extends FragmentActivity {
+public class NextActivity extends FragmentActivity implements ScoreUpdater {
 
+    //CR Move tags to Activity
+    //Extras tags
+    public static final String player1Tag = "player1";
+    public static final String player2Tag = "player2";
+
+    private String playerOneName;
+    private String playerTwoName;
+
+    private TextView scoreTextView;
     public boolean ifFirst = true;
 
     @Override
@@ -16,6 +28,13 @@ public class NextActivity extends FragmentActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.next_with_fragment);
+
+        scoreTextView = (TextView) findViewById(R.id.score);
+        Intent intent = getIntent();
+        playerOneName = intent.getStringExtra(player1Tag);
+        playerTwoName = intent.getStringExtra(player2Tag);
+
+        updateScore(0, 0);
 
         if(savedInstanceState != null) {
             return;
@@ -32,4 +51,8 @@ public class NextActivity extends FragmentActivity {
 
     }
 
+    @Override
+    public void updateScore(int playerOneScore, int playerTwoScore) {
+        scoreTextView.setText( getString(R.string.score_fmt, playerOneScore, playerTwoScore, playerOneName, playerTwoName) );
+    }
 }
