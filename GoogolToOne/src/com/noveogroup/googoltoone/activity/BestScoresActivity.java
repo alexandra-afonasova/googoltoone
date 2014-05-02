2fe0c13d148b70c260a47309b8cb5a0734847168
Anatoly Svichkarev <tolik0393@mail.ru>
2014-05-02 19:32:43 +0700

    fix problem, Best Scores Activity starts working

--- GoogolToOne/src/com/noveogroup/googoltoone/activity/BestScoresActivity.java
+++ GoogolToOne/src/com/noveogroup/googoltoone/activity/BestScoresActivity.java
@@ -29,9 +29,6 @@ public class BestScoresActivity extends Activity implements LoaderManager.Loader
 
         LoaderManager loaderManager = getLoaderManager();
 
-        // TODO: for test
-        insertData();
-
         // TODO: NAMEs for IDs
         String[] colomns = new String[]{
                 ContentDescriptor.Games.Cols.ID_PLAYER1,
@@ -50,34 +47,9 @@ public class BestScoresActivity extends Activity implements LoaderManager.Loader
         loaderManager.initLoader(0, null, this);
     }
 
-    private void insertData() {
-        Cursor cursor = getContentResolver().query(ContentDescriptor.Games.TABLE_URI, null, null, null, null);
-        int count = cursor.getCount();
-        if( count > 0 ){
-            cursor.moveToLast();
-            String id = cursor.getString(0);
-            String idpl1 = cursor.getString(1);
-            String idpl2 = cursor.getString(2);
-            String scorepl1 = cursor.getString(3);
-            String scorepl2 = cursor.getString(4);
-            String time = cursor.getString(5);
-            String time2 = cursor.getString(5);
-        }
-
-
-        ContentValues cvNewGame = new ContentValues();
-        cvNewGame.put(ContentDescriptor.Games.Cols.ID_PLAYER1, 1);
-        cvNewGame.put(ContentDescriptor.Games.Cols.ID_PLAYER2, 2);
-        cvNewGame.put(ContentDescriptor.Games.Cols.PLAYER1_SCORE, 567 );
-        cvNewGame.put(ContentDescriptor.Games.Cols.PLAYER2_SCORE, 765 );
-
-        new AsyncQueryHandler(getContentResolver()) {
-        }.startInsert(1, null, ContentDescriptor.Games.TABLE_URI, cvNewGame);
-    }
-
     @Override
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        return new CursorLoader(BestScoresActivity.this, ContentDescriptor.Players.TABLE_URI, null, null, null, null);
+        return new CursorLoader(BestScoresActivity.this, ContentDescriptor.Games.TABLE_URI, null, null, null, null);
     }
 
     @Override
